bf595518a8ab72be016495cfa3c50be457c0c3c5
Dario Emerson <dedodj@gmail.com>
2016-05-16 18:53:13 +0200

    Added new Sanderling dll

--- src/Optimat.EveOnline/MemoryStructMap.cs
+++ src/Optimat.EveOnline/MemoryStructMap.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using MemoryStruct = BotEngine.EveOnline.Interface.MemoryStruct;
+using MemoryStruct = Sanderling.Interface.MemoryStruct;
 
 namespace Optimat.EveOnline
 {
@@ -290,7 +290,7 @@ namespace Optimat.EveOnline
 			return null;
 		}
 
-		static public VonSensor.ScrollHeaderInfoFürItem AlsScrollHeader(this MemoryStruct.ListColumnHeader Header)
+		static public VonSensor.ScrollHeaderInfoFürItem AlsScrollHeader(this MemoryStruct.ColumnHeader Header)
 		{
 			if (null == Header)
 			{
